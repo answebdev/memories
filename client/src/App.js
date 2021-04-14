095ed38418f91d9ab6285460c6da3a18a6b4c30d
ANS <36783010+answebdev@users.noreply.github.com>
2021-04-13 19:30:23 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+// Code repo: https://github.com/adrianhajdin/project_mern_memories
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+};
+
+export default App;
