0f6a084ebbd741ebccfd16d528e17ab5006dc076
ANS <36783010+answebdev@users.noreply.github.com>
2021-04-16 11:25:05 -0700

    add Post component

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
 import Post from './Post/Post';
 import useStyles from './styles';
 
@@ -12,11 +13,23 @@ const Posts = () => {
   console.log(posts);
 
   return (
-    <>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </>
+    // Show loading spinner if there are no posts; otherwise, show the posts:
+    !posts.length ? (
+      <CircularProgress />
+    ) : (
+      <Grid
+        className={classes.container}
+        container
+        alignItems='stretch'
+        spacing={3}
+      >
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    )
   );
 };
 
